0bd26044410c54e8ef85338aba4e79c838f00d02
olumild <olumild998@gmail.com>
2023-08-29 11:29:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const btnEl=document.querySelector(".btn")
+const spanEl=document.getElementById('score')
+
+btnEl.onclick=()=>{
+
+    spanEl.innerText++        //spanEl.innerText=spanEl.innerText + 1
+}
+
+
