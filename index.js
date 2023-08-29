6e39a9337d3fc26e0261615328c0b12a9f18c658
olumild <olumild998@gmail.com>
2023-08-29 12:22:11 +0100

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const btnEl=document.querySelector(".btn")
+const secondBut=document.getElementById('sec')
 const spanEl=document.getElementById('score')
+
 
 btnEl.onclick=()=>{
 
     spanEl.innerText++        //spanEl.innerText=spanEl.innerText + 1
 }
 
+
+secondBut.onclick=()=>{
+    spanEl.innerText-- 
+    
+    if (spanEl.innerText <= 0){
+        spanEl.innerText=0
+    }                                 
+}
 
